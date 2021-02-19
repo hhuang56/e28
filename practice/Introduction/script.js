33f1bab5834c39c8517de95c3690df93c12b193f
Haley Huang <haley.huang.0329@gmail.com>
2021-02-18 22:32:38 -0500

    updated shopping list

--- practice/Introduction/script.js
+++ practice/Introduction/script.js
@@ -2,14 +2,23 @@
 const ShoppingList = {
     data() {
         return {
+            quantity: null,
             item: null,
             items: [],
+            total: 0
         }
     },
     methods: {
         addItem() {
             // Note how data properties are accessed via `this`
-            this.items.push(this.item);
+            this.items.push({ item: this.item, quantity: this.quantity });
+            this.total += this.quantity;
+            this.item = null;
+            this.quantity = null;
+        },
+        removeItem(index) {
+            this.total -= this.items[index].quantity;
+            this.items.splice(index, 1)
         }
     }
 }
