88a6e82691fe891e6e16c3368556f91cbba3a24c
Haley Huang <haley.huang.0329@gmail.com>
2021-03-13 19:00:46 -0500

    first draft complete

--- p1/README.md
+++ p1/README.md
@@ -1,11 +1,14 @@
-*Any instructions/notes in italics should be removed from the template before submitting* 
-
 # Project 1
-+ By: *your first and last name*
-+ Production URL: <http://e28p1.yourdomain.com>
++ By: Haley Huang
++ Production URL: <http://e28p1.heshaley.me>
 
 ## Outside resources
-*your list of outside resources go here*
+Random number between 1 and 10: http://www.java2s.com/Tutorials/Javascript/Development/Math/Get_a_random_number_between_1_and_10_in_JavaScript.htm
+
+Stock image of question mark: https://www.dlf.pt/ddetail/JioJJh_question-mark-gif-transparent-background-hd-png-download/
+
+Lists without bullets: https://www.w3schools.com/howto/howto_css_list_without_bullets.asp
+
 
 ## Notes for instructor
 *any notes for me to refer to while grading; if none, omit this section*
--- p1/script.js
+++ p1/script.js
@@ -1,61 +1,144 @@
 // Define the options of our application
-const ShoppingList = {
+const HiLow = {
     data() {
         return {
             mysteryNumber: null,
             gameCount: 0,
-            maxGuesses: 5,
-            remainingGuesses: null,
+            guesses: 10,
+            guessCount: 0,
             currentGuess: null,
-            instructions: null,
+            instructions: `Guess what number I'm thinking of between 1 and 10 (inclusive)!`,
             winning: false,
+            feedback: null,
+            scores: [],
+            history: [],
+            roundOver: false,
+            gameOver: false,
+            imageSource: '/static/questionguy.png',
+            imageAlt: 'question guy'
         }
     },
     methods: {
-        startGame() {
-            this.instructions = `Guess what number I'm thinking of between 1 and 10 (inclusive)!`;
+        startNewGame() {
+            this.feedback = '';
+            this.winning = false;
             this.currentGuess = null;
+            this.roundOver = false;
+            this.gameOver = false;
             this.mysteryNumber = Math.floor(Math.random() * 10 + 1);
             console.log(`secret number is: ${this.mysteryNumber}`);
-            this.maxGuesses = 5;
-            this.remainingGuesses = this.maxGuesses;
+            this.guesses = 10;
+            this.guessCount = 0;
             this.gameCount = 1;
+            this.scores = [];
             console.log(`gamecount is ${this.gameCount}`);
         },
         nextGame() {
-            this.instructions = `Guess what number I'm thinking of between 1 and 10 (inclusive)!`;
+            this.feedback = '';
+            this.winning = false;
+            this.roundOver = false;
             this.currentGuess = null;
             this.mysteryNumber = Math.floor(Math.random() * 10 + 1);
             console.log(`secret number is: ${this.mysteryNumber}`);
-            this.maxGuesses = this.maxGuesses - this.gameCount > 0 ? this.maxGuesses - this.gameCount : 1;
-            this.remainingGuesses = this.maxGuesses;
+            this.guesses = this.guesses === 1 ? 1 : this.guesses - 1;
+            this.guessCount = 0;
             this.gameCount++;
+            console.log(`gamecount is ${this.gameCount}`);
+        },
+        resetGame() {
+            this.feedback = '';
+            this.currentGuess = null;
+            this.roundOver = false;
+            this.gameOver = false;
+            this.mysteryNumber = Math.floor(Math.random() * 10 + 1);
+            console.log(`secret number is: ${this.mysteryNumber}`);
+            this.guesses = 10;
+            this.guessCount = 0;
+            this.gameCount = 1;
+            this.scores = [];
+            this.history = [];
             console.log(`gamecount is ${this.gameCount}`);
         },
         guessNumber() {
             console.log(`guess is ${this.currentGuess}`)
-            if (this.winning) {
-                this.instructions = "You've already won. Start a new game. :)"
-            } else if (this.currentGuess === this.mysteryNumber) {
-                this.remainingGuesses--;
-                this.winning = true;
-                this.instructions = "Congratulations, you got it!"
-            } else if (this.currentGuess < this.mysteryNumber) {
-                this.remainingGuesses--;
-                this.instructions = "Too low, try again."
-            } else if (this.currentGuess > this.mysteryNumber) {
-                this.remainingGuesses--;
-                this.instructions = "Too high, try again."
-            } else {
-                this.instructions = "I didn't quite understand you. Please try again."
-            }
+            if (!this.gameOver) {
+                if (this.currentGuess === null || this.currentGuess === '') {
+                    this.feedback = "I didn't quite understand you. Please try again."
+                } else if (this.winning) {
+                    this.feedback = "You've already won. Start a new round. :)"
+                } else if (this.currentGuess === this.mysteryNumber) {
+                    this.guesses--;
+                    this.guessCount++;
+                    this.winning = true;
+                    this.feedback = `Congratulations, you got it!`
+                    this.scores.push({ winner: 'Player', gameCount: this.gameCount, numGuesses: this.guessCount });
+                    this.roundOver = true;
+                } else if (this.currentGuess < this.mysteryNumber) {
+                    this.guesses--;
+                    this.guessCount++;
+                    this.feedback = "Too low, try again."
+                } else if (this.currentGuess > this.mysteryNumber) {
+                    this.guesses--;
+                    this.guessCount++;
+                    this.feedback = "Too high, try again."
+                } else {
+                    this.feedback = "I didn't quite understand you. Please try again."
+                }
 
-            if (this.remainingGuesses <= 0) {
-                this.instructions = `Too bad, you have no more guesses remaining. :( The secret number was ${number}. Try again by starting a new game!`;
+                if (this.guesses <= 0) {
+                    this.feedback = `Too bad, you have no more guesses remaining. :( The secret number was ${this.mysteryNumber}.`;
+                    this.scores.push({ winner: 'Computer', gameCount: this.gameCount, numGuesses: this.guessCount });
+                    this.history.push({ rounds: this.gameCount - 1 });
+                    this.roundOver = true;
+                    this.gameOver = true;
+                }
+            } else {
+                this.feedback = `This game is over. Click New Game to try again.`
             }
         }
     }
 }
 
+const GuessFeedback = {
+    name: 'GuessFeedback',
+    props: {
+        winning: {
+            type: Boolean
+        },
+        feedback: {
+            type: String
+        },
+        guesses: {
+            type: Number
+        },
+        roundOver: {
+            type: Boolean
+        },
+        gameOver: {
+            type: Boolean
+        },
+        gameCount: {
+            type: Number
+        }
+    },
+    template: '#guess-feedback',
+};
+
+const ScoreBoard = {
+    name: 'ScoreBoard',
+    props: {
+        scores: {
+            type: Array
+        },
+        history: {
+            type: Array
+        }
+    },
+    template: '#score-board',
+};
+
 // Create a new Vue instance using our options
-const app = Vue.createApp(ShoppingList).mount('#app');
+const app = Vue.createApp(HiLow);
+app.component('guess-feedback', GuessFeedback);
+app.component('score-board', ScoreBoard);
+app.mount('#app');
