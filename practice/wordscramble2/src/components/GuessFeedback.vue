35a65f7d4ebd044039f37a1559130f80e382eacf
Haley Huang <haley.huang.0329@gmail.com>
2021-03-27 16:18:43 -0400

    week 8

---
+++ practice/wordscramble2/src/components/GuessFeedback.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <span v-bind:class="{ correct: winning, error: wrong }">{{
+      feedback
+    }}</span>
+    <button v-if="winning" id="playAgain" v-on:click="$emit('start-game')">
+      Play Again
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GuessFeedback",
+  props: {
+    winning: {
+      type: Boolean,
+    },
+    wrong: {
+      type: Boolean,
+    },
+    feedback: {
+      type: String,
+    },
+  },
+};
+</script>
+
+
+<style>
+.error {
+  background-color: red;
+  color: white;
+}
+
+.correct {
+  background-color: green;
+  color: white;
+}
+</style>
---
+++ practice/wordscramble2/src/script.js
@@ -0,0 +1,105 @@
+import { createApp } from 'vue';
+import GuessFeedback from './components/GuessFeedback';
+
+// Define the options of our application
+const ShoppingList = {
+    data() {
+        return {
+            started: false,
+            playerName: "",
+            winning: false,
+            wrong: false,
+            guess: "",
+            level: "easy",
+            mysteryWord: {
+                level: "",
+                mysteryWord: "",
+                hint: "",
+            },
+            words: [
+                {
+                    level: "easy",
+                    mysteryWord: "love",
+                    hint: "Valentine"
+                },
+                {
+                    level: "easy",
+                    mysteryWord: "pencil",
+                    hint: "writing"
+                },
+                {
+                    level: "medium",
+                    mysteryWord: "apple",
+                    hint: "Red"
+                },
+                {
+                    level: "medium",
+                    mysteryWord: "washington",
+                    hint: "Rushmore"
+                },
+                {
+                    level: "hard",
+                    mysteryWord: "pumpkin",
+                    hint: "Halloween"
+                },
+                {
+                    level: "hard",
+                    mysteryWord: "football",
+                    hint: "Feet"
+                }
+            ]
+        }
+    },
+    computed: {
+        scrambledWord() {
+            return this.scramble(this.mysteryWord.mysteryWord);
+        },
+        feedback() {
+            if (this.winning) {
+                return 'You got it!'
+            } else if (this.wrong) {
+                return 'Try again!'
+            } else {
+                return '';
+            }
+        }
+    },
+    methods: {
+        startGame() {
+            this.started = true;
+            this.winning = false;
+            this.wrong = false;
+            this.guess = "";
+            let filteredList = this.words.filter(elem => elem.level === this.level);
+            let previousWord = this.mysteryWord.mysteryWord;
+            while (this.mysteryWord.mysteryWord === previousWord)
+                this.mysteryWord = filteredList[Math.floor(Math.random() * filteredList.length)]
+        },
+        guessWord() {
+            if (this.guess.toLowerCase() === this.mysteryWord.mysteryWord) {
+                this.winning = true;
+            } else {
+                this.wrong = true;
+            }
+        },
+        scramble(readableWord) {
+            // copied from github
+            // https://stackoverflow.com/questions/3943772/how-do-i-shuffle-the-characters-in-a-string-in-javascript
+            let scrambledWord = readableWord.split("");
+            let n = scrambledWord.length;
+
+            for (var i = n - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var tmp = scrambledWord[i];
+                scrambledWord[i] = scrambledWord[j];
+                scrambledWord[j] = tmp;
+            }
+            return scrambledWord.join("");
+        }
+    }
+}
+
+// Create a new Vue instance using our options
+const app = createApp(ShoppingList);
+app.component('guess-feedback', GuessFeedback);
+app.mount('#app');
